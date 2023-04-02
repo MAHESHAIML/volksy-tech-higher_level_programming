5f930568efefae2a0c269902bf13d971411731be
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-02 05:39:18 -0700

    Mahesh Enters War One Side!

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -4,27 +4,30 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-      this.print = function () {
-        for (let i = 0; i < this.height; i++) {
-          console.log('X'.repeat(this.width));
-        }
-      };
-      this.rotate = function () {
-        const fixed = this.height;
-        this.height = this.width;
-        this.width = fixed;
+    }
+  }
 
-        for (let i = 0; i < this.height; i++) {
-          console.log('X'.repeat(this.width));
-        }
-      };
-      this.double = function () {
-        this.width = w * 2;
-        this.height = h * 2;
-        for (let i = 0; i < this.height; i++) {
-          console.log('X'.repeat(this.width));
-        }
-      };
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const fixed = this.height;
+    this.height = this.width;
+    this.width = fixed;
+
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width = w * 2;
+    this.height = h * 2;
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 }
