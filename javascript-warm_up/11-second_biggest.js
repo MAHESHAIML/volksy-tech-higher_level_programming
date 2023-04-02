bffe18b2500c3c465d3e0b97f3ff2f61c7d2af65
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-02 02:26:38 -0700

    Mahesh Enters War One Side!

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 function secondBiggest (x, y, ...manyMoreArgs) {
-  console.log(manyMoreArgs);
   const descendingList = manyMoreArgs.sort(function (a, b) { return b - a; });
   console.log(descendingList[1]);
 }
 
 const allArgs = process.argv;
-console.log(allArgs);
 
 if (allArgs.length > 2) {
   secondBiggest(...allArgs);
