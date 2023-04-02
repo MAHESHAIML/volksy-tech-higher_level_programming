a9967ac0533302e6c4391dbc7f02c9a92cc187f2
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-02 05:15:43 -0700

    Mahesh Enters War One Side!

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,6 +4,11 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
+      this.print = function () {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      };
     } else {
       {}
     }
---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+      this.print = function () {
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      };
+      this.rotate = function () {
+        const fixed = this.height;
+        this.height = this.width;
+        this.width = fixed;
+
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      };
+      this.double = function () {
+        this.width = w * 2;
+        this.height = h * 2;
+        for (let i = 0; i < this.height; i++) {
+          console.log('X'.repeat(this.width));
+        }
+      };
+    } else {
+      {}
+    }
+  }
+}
+
+module.exports = Rectangle;
