ae4bef50dde59cf9eae08bf4e6ab2dd1cf6ca9c1
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-02 02:20:28 -0700

    Mahesh Enters War One Side!

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@ function add (a, b) {
   console.log(a + b);
 }
 
-let a = parseInt(process.argv[2]);
-let b = parseInt(process.argv[3]);
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 add(a, b);
