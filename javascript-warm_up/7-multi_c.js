49166ed056da34baa43a56c549c1b339ac02363c
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-02 02:09:00 -0700

    Mahesh Enters War One Side!

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const firstArg = process.argv[2];
 if (isNaN(firstArg)){
     console.log("Missing number of occurrences");
 } else {
-    for(let i = 0; i<firstArg; i++){
+    for (let i = 0; i<firstArg; i++){
         console.log("C is fun");
     }
 }
