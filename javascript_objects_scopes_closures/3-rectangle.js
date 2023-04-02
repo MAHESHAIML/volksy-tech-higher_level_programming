370e57e2fe4042608d0468b73b62a6511a3e3180
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-02 05:27:04 -0700

    Mahesh Enters War One Side!

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-      this.print = function () {
+      print() {
         for (let i = 0; i < this.height; i++) {
           console.log('X'.repeat(this.width));
         }
