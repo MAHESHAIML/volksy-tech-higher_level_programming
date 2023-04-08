ec6446c6f01c027e90f584aaf99283eb40657b68
MahiSoft95 <maheshs@brightcomgroup.com>
2023-04-07 21:34:55 -0700

    helloo

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const Sq = require('./5-square');
+class Square extends Sq {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
